45587e48beccb1afe4f1c37b17b714048a0910e0
Jasnoor Kaur <jasnoorkaurjasi@gmail.com>
2026-04-06 22:26:09 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const selected = new Set();
+
+document.querySelectorAll(".tags span").forEach(tag => {
+  tag.addEventListener("click", () => {
+    tag.classList.toggle("active");
+    const value = tag.innerText;
+
+    if (selected.has(value)) {
+      selected.delete(value);
+    } else {
+      selected.add(value);
+    }
+  });
+});
+
 
