9682ab5f73919e36ad1391edbccbefeefdc216d4
Jasnoor Kaur <jasnoorkaurjasi@gmail.com>
2026-04-06 22:26:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -20,6 +20,31 @@ function analyze() {
     perception: "Confusing but interesting.",
     reality: "You’re still figuring things out."
   };
+  if (tags.includes("productivity") && tags.includes("motivational")) {
+    persona = {
+      name: "Performative Discipline Core™",
+      perception: "Looks highly driven.",
+      reality: "Relies on bursts of motivation."
+    };
+  }
 
+  if (tags.includes("aesthetic") && tags.includes("deep")) {
+    persona = {
+      name: "Aesthetic Overthinker™",
+      perception: "Emotionally intelligent.",
+      reality: "Overanalyzes everything."
+    };
+  }
+
+  if (tags.includes("memes") && tags.includes("funny")) {
+    persona = {
+      name: "Chaos Entertainer™",
+      perception: "Funny and carefree.",
+      reality: "Avoids seriousness."
+    };
+  }
+
+
+}
 
 
